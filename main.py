d7cf71a7b8f41b297966fdbef2e8aac7b5714041
PapaJool <shakirowkamil2001@gmail.com>
2024-05-27 16:31:34 +0300

    delete cuda

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ import gdown
 
 # Путь к изображениям
 image_dir = 'data/'
-image_files = [os.path.join(image_dir, f) for f in os.listdir(image_dir) if f.endswith('.jpg')]
+image_files = [os.path.join(image_dir, f) for f in os.listdir(image_dir) if f.endswith('.png')]
 image_files = image_files[:5]
 
 # Путь к сохраненной модели и LabelEncoder
 
 # Проверка, существует ли файл
-model_path = 'path/to/save/model_with_params.pth'
+model_path = 'experiments/model_with_params.pth'
 if not os.path.exists(model_path):
     print("Model file not found. Downloading from Google Drive...")
     url = 'https://drive.google.com/uc?id=1G-p3pTZmLVL5Cq3jr7yY0M2iS_fY2DYr'
@@ -27,18 +27,21 @@ if not os.path.exists(model_path):
     gdown.download(url, model_path, quiet=False)
 encoder_path = 'experiments/label_encoder.pkl'
 
-# Инициализация извлекателя признаков
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-feature_extractor = Features1065(by=2, device=device)
+feature_extractor = Features1065(by=2, device='cpu')
 
+extracted_features = []
 # Извлечение признаков из изображений
-extracted_features = [feature_extractor._get_single_image_features(img) for img in image_files]
+for image_file in image_files:
+    # Извлекаем признаки для текущего изображения
+    image_features = feature_extractor._get_single_image_features(image_file)
+    # Добавляем метафичи в общий список
+    extracted_features.extend(image_features)
 
 # Загрузка обученной модели и LabelEncoder
 model, label_encoder = load_model_and_encoder(model_path, encoder_path)
 
 # Предсказание классов на новых данных
-predicted_labels = predict(model, extracted_features, device=device)
+predicted_labels = predict(model, extracted_features)
 predicted_classes = label_encoder.inverse_transform(predicted_labels)
 
 print("Predicted Classes:", predicted_classes)
